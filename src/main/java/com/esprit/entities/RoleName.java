63170fcdb07d77e10b98915424e9db4cad8002b5
MaryemBess <47980211+MaryemBess@users.noreply.github.com>
2021-12-08 15:45:48 +0100

    First Commit

---
+++ src/main/java/com/esprit/entities/RoleName.java
@@ -0,0 +1,10 @@
+package com.esprit.entities;
+import org.springframework.security.core.GrantedAuthority;
+public enum RoleName implements
+GrantedAuthority {
+ADMIN,
+SUPERADMIN;
+@Override
+public String getAuthority() {
+return "ROLE_" + name();
+}}
