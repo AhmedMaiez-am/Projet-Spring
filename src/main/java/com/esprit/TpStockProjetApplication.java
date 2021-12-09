755ca24faea9de7300bfbc2d1c1bdabb92f3b129
MaryemBess <47980211+MaryemBess@users.noreply.github.com>
2021-12-09 18:13:00 +0100

    Little Update

--- src/main/java/com/esprit/TpStockProjetApplication.java
+++ src/main/java/com/esprit/TpStockProjetApplication.java
@@ -9,7 +9,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 @EnableScheduling
 @SpringBootApplication
-@EnableAspectJAutoProxy
 public class TpStockProjetApplication {
 
 	public static void main(String[] args) {
